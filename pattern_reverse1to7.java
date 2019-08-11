1426651b6e240433fa673c863a114e5001fa9d63
Sairam Kamal Raj <sai.cmritcse@gmail.com>
2019-08-11 16:05:59 -0700

    Prints reverse pattern of 1 to 7.

---
+++ pattern_reverse1to7.java
@@ -0,0 +1,14 @@
+package programming;
+
+public class pattern_reverse1to7 {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=7; i++) {
+			for(int j=7; j>=i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
