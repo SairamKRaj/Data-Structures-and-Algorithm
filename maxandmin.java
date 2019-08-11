c5cc6f1b5aab9dc51cb7bbe4bd0f8ce87803b883
Sairam Kamal Raj <sai.cmritcse@gmail.com>
2019-08-11 15:59:52 -0700

    Obtaining maximum and minimum values of Integer.

---
+++ maxandmin.java
@@ -0,0 +1,11 @@
+package programming;
+
+public class MaxandMin {
+
+	public static void main(String[] args) {
+		System.out.println("Maximum Value of an integer is:"+Integer.MAX_VALUE);
+		System.out.println("Minimum Value of an integer is:"+Integer.MIN_VALUE);
+
+	}
+
+}
