ed9fc8e7fef44f8065f409cdcea3233ad2fa3ec0
Sairam Kamal Raj <sai.cmritcse@gmail.com>
2019-08-11 15:44:48 -0700

    Obtain ASCII Values of Characters.

---
+++ characterValues.java
@@ -0,0 +1,23 @@
+package programming;
+
+public class characterValues {
+
+	public static void main(String[] args) {
+		char c1='a';  
+		char c2='z';  
+		int a=c1;  
+		int b=c2;  
+		System.out.println(a);  
+		System.out.println(b);
+		
+		
+		char c3='A';  
+		char c4='Z';  
+		int c=c3;  
+		int d=c4;  
+		System.out.println(c);  
+		System.out.println(d);
+			
+	}
+
+}
